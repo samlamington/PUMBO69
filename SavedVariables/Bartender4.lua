2ed0f6caea3fbf44e59ab00c4738c926d9bc9254
unknown <wida_wada@hotmail.com>
2018-07-20 21:48:49 +1000

    Bindings update.

---
+++ SavedVariables/Bartender4.lua
@@ -0,0 +1,216 @@
+
+Bartender4DB = {
+	["namespaces"] = {
+		["StatusTrackingBar"] = {
+		},
+		["ActionBars"] = {
+			["profiles"] = {
+				["Wandoo - Dreadmaul"] = {
+					["actionbars"] = {
+						{
+							["version"] = 3,
+							["padding"] = 6,
+							["position"] = {
+								["x"] = -510,
+								["point"] = "BOTTOM",
+								["y"] = 41.75,
+							},
+						}, -- [1]
+						{
+							["enabled"] = false,
+							["version"] = 3,
+							["position"] = {
+								["x"] = -231.500091552734,
+								["point"] = "CENTER",
+								["y"] = -227.500030517578,
+							},
+						}, -- [2]
+						{
+							["rows"] = 12,
+							["version"] = 3,
+							["position"] = {
+								["x"] = -82,
+								["point"] = "BOTTOMRIGHT",
+								["y"] = 610,
+							},
+							["padding"] = 5,
+						}, -- [3]
+						{
+							["rows"] = 12,
+							["version"] = 3,
+							["position"] = {
+								["x"] = -42,
+								["point"] = "BOTTOMRIGHT",
+								["y"] = 610,
+							},
+							["padding"] = 5,
+						}, -- [4]
+						{
+							["version"] = 3,
+							["position"] = {
+								["x"] = 3,
+								["point"] = "BOTTOM",
+								["y"] = 94,
+							},
+							["padding"] = 6,
+						}, -- [5]
+						{
+							["version"] = 3,
+							["position"] = {
+								["x"] = -510,
+								["point"] = "BOTTOM",
+								["y"] = 94,
+							},
+							["padding"] = 6,
+						}, -- [6]
+						{
+						}, -- [7]
+						{
+							["enabled"] = true,
+							["buttons"] = 2,
+							["version"] = 3,
+							["position"] = {
+								["x"] = -83.0000305175781,
+								["point"] = "CENTER",
+								["scale"] = 2,
+								["y"] = -274.313400268555,
+							},
+						}, -- [8]
+						{
+							["enabled"] = true,
+							["version"] = 3,
+							["position"] = {
+								["x"] = -575,
+								["point"] = "CENTER",
+								["y"] = 145,
+							},
+						}, -- [9]
+						{
+							["enabled"] = true,
+							["version"] = 3,
+							["position"] = {
+								["x"] = 100,
+								["point"] = "CENTER",
+								["y"] = 145,
+							},
+						}, -- [10]
+					},
+				},
+			},
+		},
+		["LibDualSpec-1.0"] = {
+		},
+		["ExtraActionBar"] = {
+			["profiles"] = {
+				["Wandoo - Dreadmaul"] = {
+					["version"] = 3,
+					["position"] = {
+						["x"] = 443.403564453125,
+						["point"] = "BOTTOM",
+						["y"] = 174.339920043945,
+					},
+				},
+			},
+		},
+		["MicroMenu"] = {
+			["profiles"] = {
+				["Wandoo - Dreadmaul"] = {
+					["version"] = 3,
+					["padding"] = -2,
+					["position"] = {
+						["x"] = 37.5,
+						["point"] = "BOTTOM",
+						["scale"] = 1,
+						["y"] = 41.75,
+					},
+				},
+			},
+		},
+		["BagBar"] = {
+			["profiles"] = {
+				["Wandoo - Dreadmaul"] = {
+					["version"] = 3,
+					["position"] = {
+						["x"] = 345,
+						["point"] = "BOTTOM",
+						["y"] = 38.5,
+					},
+				},
+			},
+		},
+		["BlizzardArt"] = {
+			["profiles"] = {
+				["Wandoo - Dreadmaul"] = {
+					["enabled"] = true,
+					["version"] = 3,
+					["position"] = {
+						["x"] = -512,
+						["point"] = "BOTTOM",
+						["y"] = 47,
+					},
+				},
+			},
+		},
+		["ZoneAbilityBar"] = {
+			["profiles"] = {
+				["Wandoo - Dreadmaul"] = {
+					["version"] = 3,
+					["position"] = {
+						["x"] = 442.7705078125,
+						["point"] = "BOTTOM",
+						["y"] = 248.580032348633,
+					},
+				},
+			},
+		},
+		["StanceBar"] = {
+			["profiles"] = {
+				["Wandoo - Dreadmaul"] = {
+					["version"] = 3,
+					["position"] = {
+						["x"] = -82.5000305175781,
+						["point"] = "CENTER",
+						["y"] = -14.9999771118164,
+					},
+				},
+			},
+		},
+		["PetBar"] = {
+			["profiles"] = {
+				["Wandoo - Dreadmaul"] = {
+					["version"] = 3,
+					["position"] = {
+						["x"] = -470.793273925781,
+						["point"] = "BOTTOM",
+						["y"] = 128.000030517578,
+					},
+				},
+			},
+		},
+		["Vehicle"] = {
+			["profiles"] = {
+				["Wandoo - Dreadmaul"] = {
+					["version"] = 3,
+					["position"] = {
+						["x"] = -509.299957275391,
+						["point"] = "BOTTOM",
+						["y"] = 130.000015258789,
+					},
+				},
+			},
+		},
+	},
+	["profileKeys"] = {
+		["Wandoo - Dreadmaul"] = "Wandoo - Dreadmaul",
+	},
+	["profiles"] = {
+		["Wandoo - Dreadmaul"] = {
+			["focuscastmodifier"] = false,
+			["minimapIcon"] = {
+				["minimapPos"] = 190.611841259778,
+			},
+			["blizzardVehicle"] = true,
+			["outofrange"] = "hotkey",
+		},
+	},
+}
